24b3973eda8c638098f4bc72695499bf49eee610
ProjectMyst <ge@myst.uk.net>
2021-04-25 14:38:05 +0100

    Update ApiCalls.php

--- ApiCalls.php
+++ ApiCalls.php
@@ -1,5 +1,42 @@
 <?php
 
+$rcmail = rcmail::get_instance();
+$user = $rcmail->user;
+$storage = $rcmail->get_storage();
+
+if (filter_var($this->data['user'],FILTER_VALIDATE_EMAIL)) {
+	$email_user = $this->data['user'];
+	$email_domain = array_pop(explode('@', $email_user));
+	$email_name = substr($email_user, 0, strrpos($email_user, '@'));
+}
+
+$ip = $_SERVER["REMOTE_ADDR"];
+$url = 'https://' . $_SERVER["SERVER_NAME"]/** . $_SERVER["REQUEST_URI"]**/;
+
+$plesk_host = $rcmail->config->get('plesk_host');
+$plesk_login = $rcmail->config->get('plesk_login');
+$plesk_password = $rcmail->config->get('plesk_password');
+
+$plesk_client = new PleskApiClient($plesk_host);
+$plesk_client->setCredentials($plesk_login, $plesk_password);
+
+$plek_create_session = <<<EOF
+<packet>
+	<server>
+		<create_session>
+			<login>$email_user</login>
+			<data>
+				<user_ip>$ip</user_ip>
+				<source_server>$url</source_server>
+			</data>
+		</create_session>
+	</server>
+</packet>
+EOF;
+
+$plesk_session_response = new SimpleXMLElement($plesk_client->request($plek_create_session));
+$plesk_client_session = $plesk_session_response->server->create_session->result->id;
+
 $plek_domain = <<<EOF
 <packet>
 	<site>
